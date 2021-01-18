b9eb507be664870ca47023f8d2656df48200ca43
leo <leoblai@etud.univ-angers.fr>
2021-01-18 10:11:25 +0100

    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,17 @@
 
 int main()
 {
-    Road road(100);
+    Road road(20);
     //road.show();
     Bike bike;
     bike.run(road);
+
+    Road road(40);
+    //road.show();
+    Bike bike;
+    bike.run(longRoad);
+
     return 0;
 }
+
+
